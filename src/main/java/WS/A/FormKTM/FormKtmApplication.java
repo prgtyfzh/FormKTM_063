bee44e73ee4317790a8ac0fde8c6d479afa55140
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-11-12 16:22:50 +0700

    First Commit

---
+++ src/main/java/WS/A/FormKTM/FormKtmApplication.java
@@ -0,0 +1,13 @@
+package WS.A.FormKTM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormKtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormKtmApplication.class, args);
+	}
+
+}
