10f7a267a97b2b8a20dda72061e1b034a0e0bd42
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-11-12 17:53:16 +0700

    Menambahan controller dan mengisinya

---
+++ src/main/java/WS/A/FormKTM/ControllerProject.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WS.A.FormKTM;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author ASUS
+ */
+
+@Controller
+public class ControllerProject {
+    
+    @RequestMapping("/mainpage")
+    //@ResponseBody
+    
+    public String getData(@RequestParam("nama") String nama,
+            @RequestParam("nim") String nim,
+            @RequestParam("gambar") MultipartFile img,
+            @RequestParam("ttl")@DateTimeFormat(pattern="yyyy-MM-dd") Date date,
+            Model kiriman
+            ) throws IOException {
+        
+        SimpleDateFormat newTanggal = new SimpleDateFormat("dd-MMMMM-yyyy");
+        String tanggalKu = newTanggal.format(date);
+        
+        String blob = Base64.encodeBase64String(img.getBytes());      
+        String isiimg = "data:image/*;base64, "+blob+" ";
+        
+        kiriman.addAttribute("paketnama", nama);
+        kiriman.addAttribute("paketnim", nim);
+        kiriman.addAttribute("pakettgl", tanggalKu);
+        kiriman.addAttribute("gambar", isiimg);
+        
+        return "viewpage";
+    }
+}
